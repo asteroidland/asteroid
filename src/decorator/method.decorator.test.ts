c2c654fe178ea74b8a77fc29dac12328da8d370b
edualb <eduardoslafiq@gmail.com>
2020-12-10 14:11:31 -0300

    add POST, DELETE, PUT and PATCH methods

--- src/decorator/method.decorator.test.ts
+++ src/decorator/method.decorator.test.ts
@@ -2,7 +2,7 @@ import { assertEquals, assertNotEquals } from "https://deno.land/std@0.80.0/test
 import { ControllerMetadata, HTTPMethod } from "../types.ts";
 import { ReflectUtils } from "../utils/reflect.utils.ts";
 import { Controller } from "./controller.decorator.ts";
-import { Get } from "./method.decorator.ts";
+import { Delete, Get, Patch, Post, Put } from "./method.decorator.ts";
 
 const testPackage = 'Method Decorator'
 
@@ -42,6 +42,166 @@ Deno.test(`[${testPackage}] #2 - Method with : Should be metadata routes and Met
   for (let key of metadata!.routes.keys()) {
     assertEquals(key.path, '');
     assertEquals(key.method, HTTPMethod.GET);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #3 - Method with : Should be metadata routes and MethodOptions{path: myPostTest, method: POST}`, () => {
+  @Controller()
+  class MethodTest {
+    @Post('myPostTest')
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, 'myPostTest');
+    assertEquals(key.method, HTTPMethod.POST);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #4 - Method with : Should be metadata routes and MethodOptions{path: '', method: POST}`, () => {
+  @Controller()
+  class MethodTest {
+    @Post()
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, '');
+    assertEquals(key.method, HTTPMethod.POST);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #5 - Method with : Should be metadata routes and MethodOptions{path: myPutTest, method: PUT}`, () => {
+  @Controller()
+  class MethodTest {
+    @Put('myPutTest')
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, 'myPutTest');
+    assertEquals(key.method, HTTPMethod.PUT);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #6 - Method with : Should be metadata routes and MethodOptions{path: '', method: PUT}`, () => {
+  @Controller()
+  class MethodTest {
+    @Put()
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, '');
+    assertEquals(key.method, HTTPMethod.PUT);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #7 - Method with : Should be metadata routes and MethodOptions{path: myDeleteTest, method: DELETE}`, () => {
+  @Controller()
+  class MethodTest {
+    @Delete('myDeleteTest')
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, 'myDeleteTest');
+    assertEquals(key.method, HTTPMethod.DELETE);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #8 - Method with : Should be metadata routes and MethodOptions{path: '', method: DELETE}`, () => {
+  @Controller()
+  class MethodTest {
+    @Delete()
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, '');
+    assertEquals(key.method, HTTPMethod.DELETE);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #9 - Method with : Should be metadata routes and MethodOptions{path: myPatchTest, method: PATCH}`, () => {
+  @Controller()
+  class MethodTest {
+    @Patch('myPatchTest')
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, 'myPatchTest');
+    assertEquals(key.method, HTTPMethod.PATCH);
+  }
+  
+});
+
+Deno.test(`[${testPackage}] #10 - Method with : Should be metadata routes and MethodOptions{path: '', method: DELETE}`, () => {
+  @Controller()
+  class MethodTest {
+    @Patch()
+    getTest() {}
+  }
+
+  const metadata: ControllerMetadata | undefined = ReflectUtils.getOwnControllerMetadata(MethodTest);
+
+  assertNotEquals(metadata, undefined);
+  assertEquals(metadata?.prefix, '/');
+  assertEquals(metadata?.routes.size, 1);
+
+  for (let key of metadata!.routes.keys()) {
+    assertEquals(key.path, '');
+    assertEquals(key.method, HTTPMethod.PATCH);
   }
   
 });
--- src/decorator/method.decorator.ts
+++ src/decorator/method.decorator.ts
@@ -1,15 +1,64 @@
 import { ControllerMetadata, HTTPMethod, MethodOptions } from "../types.ts";
 import { ReflectUtils } from "../utils/reflect.utils.ts";
 
+function createMethodOptions(path: string | undefined, method: HTTPMethod): MethodOptions {
+    const endpoint: string = path !== undefined ? path : ''
+    const options: MethodOptions = {
+        path: endpoint,
+        method: method
+    };
+    return options
+}
+
 export function Get(path?: string): MethodDecorator {
     return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void => {
         const metadata: ControllerMetadata = ReflectUtils.getControllerMetadata(target) ?? ReflectUtils.getDefaultControllerMetadata();
 
-        const endpoint: string = path !== undefined ? path : ''
-        const options: MethodOptions = {
-            path: endpoint,
-            method: HTTPMethod.GET
-        };
+        const options: MethodOptions = createMethodOptions(path, HTTPMethod.GET)
+
+        metadata.routes.set(options, descriptor.value);
+        ReflectUtils.setControllerMetadata(metadata, target);
+    }
+}
+
+export function Post(path?: string): MethodDecorator {
+    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void => {
+        const metadata: ControllerMetadata = ReflectUtils.getControllerMetadata(target) ?? ReflectUtils.getDefaultControllerMetadata();
+
+        const options: MethodOptions = createMethodOptions(path, HTTPMethod.POST)
+
+        metadata.routes.set(options, descriptor.value);
+        ReflectUtils.setControllerMetadata(metadata, target);
+    }
+}
+
+export function Put(path?: string): MethodDecorator {
+    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void => {
+        const metadata: ControllerMetadata = ReflectUtils.getControllerMetadata(target) ?? ReflectUtils.getDefaultControllerMetadata();
+
+        const options: MethodOptions = createMethodOptions(path, HTTPMethod.PUT)
+
+        metadata.routes.set(options, descriptor.value);
+        ReflectUtils.setControllerMetadata(metadata, target);
+    }
+}
+
+export function Delete(path?: string): MethodDecorator {
+    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void => {
+        const metadata: ControllerMetadata = ReflectUtils.getControllerMetadata(target) ?? ReflectUtils.getDefaultControllerMetadata();
+
+        const options: MethodOptions = createMethodOptions(path, HTTPMethod.DELETE)
+
+        metadata.routes.set(options, descriptor.value);
+        ReflectUtils.setControllerMetadata(metadata, target);
+    }
+}
+
+export function Patch(path?: string): MethodDecorator {
+    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void => {
+        const metadata: ControllerMetadata = ReflectUtils.getControllerMetadata(target) ?? ReflectUtils.getDefaultControllerMetadata();
+
+        const options: MethodOptions = createMethodOptions(path, HTTPMethod.PATCH)
 
         metadata.routes.set(options, descriptor.value);
         ReflectUtils.setControllerMetadata(metadata, target);
